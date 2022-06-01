c970fd8fb79f6bc787ef67b1082d3d3005f0d26d
Linhvjc <93339285+Linhvjc@users.noreply.github.com>
2022-06-01 16:08:44 +0700

    first commit

---
+++ src/Entity/Students.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudentsRepository::class)]
+class Students
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $Name;
+
+    #[ORM\Column(type: 'date')]
+    private $DoB;
+
+    #[ORM\Column(type: 'string', length: 200, nullable: true)]
+    private $Address;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getDoB(): ?\DateTimeInterface
+    {
+        return $this->DoB;
+    }
+
+    public function setDoB(\DateTimeInterface $DoB): self
+    {
+        $this->DoB = $DoB;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->Address;
+    }
+
+    public function setAddress(?string $Address): self
+    {
+        $this->Address = $Address;
+
+        return $this;
+    }
+}
